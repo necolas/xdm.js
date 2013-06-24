03e39ff3726fd8e04ac7ac8d5d223bb20d1b3668
Nicolas Gallagher <nicolasgallagher@gmail.com>
2013-06-24 01:31:54 -0700

    Add unit tests for `xdm` and `xdm.Rpc`
    Unit tests for the core interface.

---
+++ test/spec/xdm.Rpc.spec.js
@@ -0,0 +1,188 @@
+describe('an instance of xdm.Rpc', function () {
+    'use strict';
+
+    var expectedMessage;
+    var guest;
+    var i = 0;
+    var isReady = false;
+
+    beforeEach(function () {
+        // create a unique message
+        expectedMessage = ++i + "_abcd1234%@¤/";
+
+        // set up the cross-domain messaging channel
+        guest = new xdm.Rpc({
+            remote: '/base/test/fixture/guest.html',
+            container: document.body,
+            props: {
+                height: '50px'
+            },
+            onReady: function () {
+                isReady = true;
+            }
+        }, {
+            remote: {
+                voidMethod: {},
+                asyncMethod: {},
+                regularMethod: {},
+                errorMethod: {},
+                nonexistent: {},
+                namedParamsMethod: {
+                    namedParams: true
+                }
+            },
+            local: {
+                voidCallback: {
+                    method: function (message) {
+                        //scope.notifyResult((scope.expectedMessage === message));
+                    }
+                }
+            }
+        });
+
+        // wait until the onReady callback has been triggered
+        waitsFor(function () {
+            return isReady;
+        });
+    });
+
+    afterEach(function () {
+        isReady = false;
+        guest.destroy();
+    });
+
+    it('creates an iframe', function () {
+        expect(document.body.innerHTML).toContain('iframe');
+    });
+
+    it('has a reference to the iframe', function () {
+        expect(guest.iframe.nodeName).toEqual('IFRAME');
+    });
+
+    it('triggers onReady', function () {
+        expect(isReady).toBe(true);
+    });
+
+    it('can remove the iframe', function () {
+        guest.destroy();
+        expect(document.getElementsByTagName('iframe').length).toBe(0);
+    });
+
+    it('can remove the channel', function () {
+        guest.destroy();
+        expect(guest.voidMethod).not.toBeDefined();
+    });
+
+    /**
+     * Remote procedure call tests
+     */
+
+    describe('remote method call', function () {
+        var message;
+        var isComplete = false;
+
+        afterEach(function () {
+            isComplete = false;
+            message = null;
+        });
+
+        it('supports void methods', function () {
+            expect(guest.voidMethod(expectedMessage));
+        });
+
+        it('supports async methods', function () {
+            runs(function () {
+                guest.asyncMethod(expectedMessage, function (msg) {
+                    message = msg;
+                    isComplete = true;
+                });
+            });
+
+            waitsFor(function () {
+                return isComplete;
+            });
+
+            runs(function () {
+                expect(message).toEqual(expectedMessage);
+            });
+        });
+
+        it('supports regular methods', function () {
+            runs(function () {
+                guest.regularMethod(expectedMessage, function (msg) {
+                    message = msg;
+                    isComplete = true;
+                });
+            });
+
+            waitsFor(function () {
+                return isComplete;
+            });
+
+            runs(function () {
+                expect(message).toEqual(expectedMessage);
+            });
+        });
+
+        it('handles errors by calling the error callback', function () {
+            var isError = false;
+
+            runs(function () {
+                guest.errorMethod(expectedMessage, function (msg) {
+                    isComplete = true;
+                }, function (err) {
+                    isError = true;
+                    isComplete = true;
+                });
+            });
+
+            waitsFor(function () {
+                return isComplete;
+            });
+
+            runs(function () {
+                expect(isError).toBe(true);
+            });
+        });
+
+        it('throws an error on nonexistant methods', function () {
+            var isError = false;
+
+            runs(function () {
+                guest.errorMethod(expectedMessage, function (msg) {
+                    isComplete = true;
+                }, function (err) {
+                    isError = true;
+                    isComplete = true;
+                });
+            });
+
+            waitsFor(function () {
+                return isComplete;
+            });
+
+            runs(function () {
+                expect(isError).toBe(true);
+            });
+        });
+
+        it('supports messages with named parameters', function () {
+            runs(function () {
+                guest.namedParamsMethod({
+                    msg: expectedMessage
+                }, function (msg) {
+                    message = msg;
+                    isComplete = true;
+                });
+            });
+
+            waitsFor(function () {
+                return isComplete;
+            });
+
+            runs(function () {
+                expect(message).toEqual(expectedMessage);
+            });
+        });
+    });
+});
---
+++ test/spec/xdm.spec.js
@@ -0,0 +1,36 @@
+describe('the xdm object', function () {
+    'use strict';
+
+    it('is defined', function () {
+        expect(xdm).toBeDefined();
+    });
+
+    it('exposes xdm.query', function () {
+        expect(xdm.query).toBeDefined();
+    });
+
+    it('exposes xdm.version', function () {
+        expect(xdm.version).toBeDefined();
+    });
+
+    it('exposes xdm.stack', function () {
+        expect(xdm.stack).toBeDefined();
+    });
+
+    it('exposes xdm.stack.PostMessageTransport', function () {
+        expect(xdm.stack.PostMessageTransport).toBeDefined();
+    });
+
+    it('exposes xdm.stack.QueueBehavior', function () {
+        expect(xdm.stack.QueueBehavior).toBeDefined();
+    });
+
+    it('exposes xdm.stack.RpcBehavior', function () {
+        expect(xdm.stack.RpcBehavior).toBeDefined();
+    });
+
+    it('exposes xdm.Rpc', function () {
+        expect(xdm.Rpc).toBeDefined();
+    });
+});
+
